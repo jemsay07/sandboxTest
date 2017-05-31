9061e8f5e60c6fd459b71f75f9ce3573599d28bd
jem say <jem07.say@gmail.com>
2017-05-31 09:41:21 +0800

    test

---
+++ app/user.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class user extends Model{
+    /*public function passport(){
+    	return $this->hasOne(Passport::class);
+    }*/
+
+/*    public function mobile(){
+    	return $this->hasMany(Mobile::class);
+    }*/
+    public function roles(){
+    	return $this->belongsToMany(role::class);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.masters')
+
+@section('content')
+    <!-- <select> -->
+       <!--  @foreach($mobiles as $mobile)
+           <option>
+               {{ $mobile->number }}
+           </option>
+       @endforeach -->
+   <!--  </select> -->
+@stop
